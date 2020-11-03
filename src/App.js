cb1a85b58ee69a61e765dc285ab13e362c2c3476
Khabibullo Saydullaev <khabibullosaydullaev@gmail.com>
2020-11-03 15:05:31 +0500

    added pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from "react";
+import "./App.css";
+import Home from "./pages/Home";
+import Rooms from "./pages/Rooms";
+import SingleRoom from "./pages/SingleRoom";
+import Error from "./pages/Error";
 
 function App() {
-    return <h1>hello world</h1>;
+    return (
+        <div>
+            <Home />
+            <Rooms />
+            <SingleRoom />
+            <Error />
+        </div>
+    );
 }
 
 export default App;
